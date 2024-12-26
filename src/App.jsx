5a0225acc00d1618c508cf7acdb1370ea27ad0f5
rmsetty <rajiv.dfw@gmail.com>
2024-12-26 17:14:36 -0600

    Added a form to submit a new user

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import { supabase } from './createClient'
 
 const App = () => {
   const [users, setUsers] = useState([])
+  const [user, setUser] = useState({
+    name:'', age:''
+  })
+
   console.log(users)
   useEffect(() => {
     fetchUsers()
@@ -13,10 +17,42 @@ const App = () => {
     .from('users')
     .select('*')
     setUsers(data)
-
   }
+
+  function handleChange(event){
+    setUser(prevFormData => {
+      return {
+        ...prevFormData,
+      [event.target.name]:event.target.value
+      }
+    })
+  }
+
+  async function createUser(){
+    const { error } = await supabase
+  .from('users')
+  .insert({ name: user.name, age: user.age })
+  }
+
   return (
     <div>
+      <form onSubmit={createUser}>
+        <input
+          type="text"
+          placeholder="Name"
+          name='name'
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          placeholder="Age"
+          name='age'
+          onChange={handleChange}
+        />
+        <button type="submit">
+          Create
+        </button>
+      </form>
       <table>
         <thead>
           <tr>
@@ -28,7 +64,7 @@ const App = () => {
 
         <tbody>
           {users.map((user) =>           
-            <tr>
+            <tr key={user.id}>
               <td>{user.id}</td>
               <td>{user.name}</td>
               <td>{user.age}</td>
