975d89cab2e4688b8a6951af83d6c0e884e2dc9a
rmsetty <rajiv.dfw@gmail.com>
2024-12-26 16:16:26 -0600

    Set up table, styled, and connected to database

--- src/App.jsx
+++ src/App.jsx
@@ -17,12 +17,25 @@ const App = () => {
   }
   return (
     <div>
-      <h1>Users</h1>
-      <ul>
-        {users.map(user => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {users.map((user) =>           
+            <tr>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.age}</td>
+            </tr> 
+          )}
+        </tbody>
+      </table>
     </div>
   )}
 
